262231d60dfc358c5b7e5ac23b6578057d868c8b
CKerko <59737776+cKerko@users.noreply.github.com>
2026-06-15 17:02:24 +0000

    061526-noon

--- 8 - Accessibility, performance and best practices/Challenge/Challenge/script.js
+++ 8 - Accessibility, performance and best practices/Challenge/Challenge/script.js
@@ -3,4 +3,9 @@
 // 2. Identify areas for improvement.
 // 3. Implement changes to enhance accessibility.
 // 4. Test the updated page using tools like screen readers or accessibility checkers.
-
+document.addEventListener('DOMContentLoaded', () => {
+    const actionButton = document. getElementById('actionButton');
+    actionButton.addEventListener('click', () => {
+        alert("Clicked");
+    });
+});
